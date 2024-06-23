ca738d4a5b4179a50d35726f3a47b4beec338227
harleysherman <harsatr11@icloud.com>
2024-06-22 22:19:50 -0500

    rewrote vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  // plugins: [react()],
+  // server: {
+  //   port: 3000,
+  //   open: true,
+  //   proxy: {
+  //     '/graphql': {
+  //       target: 'http://localhost:3001',
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   }
+  //   },
   plugins: [react()],
   server: {
+    host: true,
+    strictPort: true,
     port: 3000,
-    open: true,
-    // proxy: {
-    //   '/graphql': {
-    //     target: 'http://localhost:3001',
-    //     changeOrigin: true,
-    //     secure: false,
-    //   },
-    // }
-    },
-  });
+  },
+});
